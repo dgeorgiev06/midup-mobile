922c8c4112690f4a17811e7ea52f47662f67a2d8
dgeorgiev06 <33109300+dgeorgiev06@users.noreply.github.com>
2019-01-20 13:12:39 -0500

    Update AppConfig.js

--- App/Config/AppConfig.js
+++ App/Config/AppConfig.js
@@ -3,5 +3,5 @@
 export default {
   apiUrl: 'http://192.168.1.3:8080/',
   appUrlScheme: 'midup',
-  mapboxAccessToken: 'pk.eyJ1IjoiZGdlb3JnaWV2MDYiLCJhIjoiY2pvZHp3ZzlrMTIwdTN2bzE0ajBxMmowbiJ9.Vv_Ou9b27nnYqM1ecCkrGg'
+  mapboxAccessToken: ''
 }
